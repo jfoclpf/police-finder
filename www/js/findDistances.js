662192bf76c16f20795f92b58277b6d7264accc2
jfoclpf <joao.pimentel.ferreira@gmail.com>
2021-01-21 21:42:46 +0100

    update

--- www/js/findDistances.js
+++ www/js/findDistances.js
@@ -1,6 +1,15 @@
 /* global app, $, google */
 
 app.findDistances = (function (thisModule) {
+  $('#refresh-authorities').click((e) => {
+    $('#authorities_list').empty()
+    const googleMapsKey = app.localization.getGoogleMapsKey()
+    const coordinates = app.localization.getCoordinates()
+    const latitude = coordinates.latitude
+    const longitude = coordinates.longitude
+    calculateDistancesToAuthorities(latitude, longitude, googleMapsKey)
+  })
+
   function calculateDistancesToAuthorities (latitude, longitude, googleMapsKey) {
     // fetch contacts of police authorities
     app.contactsFunctions.loadPoliceContacts(function (err, contacts) {
@@ -33,11 +42,21 @@ app.findDistances = (function (thisModule) {
             }
           }
 
+          if (selectedAuthorities.length === 0) {
+            showNoResults('Sem resultados relevantes, considerando a sua localização')
+            return
+          }
+
           console.log(selectedAuthorities)
           const googleMapsApiDistancesUrl = 'https://maps.googleapis.com/maps/api/distancematrix/json'
           $.get(`${googleMapsApiDistancesUrl}?key=${googleMapsKey}&language=pt-PT&origins=${latitude},${longitude}&destinations=${selectedAuthoritiesCoords.join('|')}`,
             function (result) {
               console.log('Distances:', result)
+              if (result.rows.length === 0) {
+                showNoResults('Sem resultados relevantes, considerando a sua localização')
+                return
+              }
+
               var elements = result.rows[0].elements
               for (let i = 0; i < selectedAuthorities.length; i++) {
                 selectedAuthorities[i].distance = elements[i].distance
@@ -48,6 +67,7 @@ app.findDistances = (function (thisModule) {
             })
         } else {
           console.error('error on reverseGeocoder')
+          showNoResults('Erro no serviço de geolocalização')
         }
       })
     })
@@ -65,6 +85,10 @@ app.findDistances = (function (thisModule) {
       return 0
     })
     console.log(authoritiesSortedByDistance)
+
+    $('#authorities_list').find('*').off() // removes all event handlers
+    $('#authorities_list').empty()
+
     for (let i = 0; i < authoritiesSortedByDistance.length && i < 5; i++) {
       const authority = authoritiesSortedByDistance[i]
       $('#authorities_list').append(`
@@ -74,11 +98,39 @@ app.findDistances = (function (thisModule) {
           <b>Distância</b>: ${authority.distance.text}<br>
           ${authority.horario ? '<b>Horário</b>: ' + authority.horario + '<br>' : ''}
           <b>Morada</b>: ${authority.morada}, <span style="white-space: nowrap;">${authority.codigopostal} ${authority.localidadepostal}</span><br>
-          <b>Telefone</b>: ${authority.telefone}&nbsp;&nbsp;&nbsp;<button type="button" class="fa fa-phone btn btn-success btn-lg" id="phone_authority"></button><br>
-          <b>E-mail</b>: ${authority.email}&nbsp;&nbsp;&nbsp;<button type="button" class="fa fa-envelope btn btn-success btn-lg" id="send_email_to_authority"></button><br>
+          <b>Telefone</b>: ${authority.telefone}<br>
+          <b>E-mail</b>: ${authority.email}
+          <div class="d-flex justify-content-around">
+            <button type="button" class="mt-2 btn-authorities-list fa fa-phone btn btn-success" id="phone_authority"></button>
+            <button type="button" class="mt-2 btn-authorities-list fa fa-envelope btn btn-success" id="send_email_to_authority"></button>
+          </div>
         </div>`
       )
     }
+  }
+
+  function showNoResults (message) {
+    $('#authorities_list').find('*').off() // removes all event handlers
+    $('#authorities_list').empty()
+    $('#authorities_list').append(`<center>${message}</center>`)
+  }
+
+  function getDistanceFromLatLonInKm (lat1, lon1, lat2, lon2) {
+    var R = 6371 // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1) // deg2rad below
+    var dLon = deg2rad(lon2 - lon1)
+    var a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2)
+
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    var d = R * c // Distance in km
+    return d
+  }
+
+  function deg2rad (deg) {
+    return deg * (Math.PI / 180)
   }
 
   thisModule.calculateDistancesToAuthorities = calculateDistancesToAuthorities
